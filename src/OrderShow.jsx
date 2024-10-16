4b973f4d922f1a127b34f11e01a9bfcb8ba7ecad
waykwo <72991933+waykwo@users.noreply.github.com>
2024-10-16 12:10:16 -0400

    OrderShow now displays individual product info for the order

--- src/OrderShow.jsx
+++ src/OrderShow.jsx
@@ -2,10 +2,32 @@ import { useLoaderData } from "react-router-dom"
 
 export function OrderShow() {
   const order = useLoaderData();
+  console.log(order);
 
   return (
     <div>
-      <p>Order: {JSON.stringify(order)}</p>
+      {/* <div>
+        <p>Order: {JSON.stringify(order)}</p>      
+      </div> */}
+      <div>
+        <ul>
+          <li>ID: {order.id}</li>
+          <li>Subtotal: {order.subtotal}</li>
+          <li>Tax: {order.tax}</li>
+          <li>Total: {order.total}</li>
+          <li>Date and Time: {order.order_datetime}</li>
+          <li>Items:</li>
+          {/* <li>Product: {order.carted_products[0].product.name}</li> */}
+          {order.carted_products.map(cp => (
+            <ul key={cp.id}>
+              <li>Product: {cp.product.name}</li>
+              <li>Quantity: {cp.quantity}</li>
+              <li>Price: {cp.product.price}</li>
+              <hr />
+            </ul>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
